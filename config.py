0c150bb6c1e5c71ede33ac8f07181ade863dd1c1
Hasnain Zaz <zazhasnain@gmail.com>
2025-10-21 21:03:45 +0530

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-REDDIT_CLIENT_ID = os.getenv('jYHQ0TPCSbIGXB3Ec9ag0A')
-REDDIT_CLIENT_SECRET = os.getenv('iCpCnkmy2SlpYYP3wTfc5iR2m7CWOA')
-REDDIT_USER_AGENT = os.getenv('android:com.example.myredditapp:v1.2.3 (by u/Patient_List_5224)', 'reddit-sentiment-app')
